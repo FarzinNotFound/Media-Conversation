508fad14c4118331752dbf6cdd9ea66ae8ff79c0
Farzin <79421881+FarzinNotFound@users.noreply.github.com>
2021-10-07 16:59:24 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-sudo = [0000] # your user id 
-api_id = 0000 # your api id
-api_hash = 0000 # your api hash
-token = 'token' # your token
+sudo = [0000] # put your user id 
+api_id = 0000 # put your api id
+api_hash = 0000 # put your api hash
+token = 'token' # put your token
